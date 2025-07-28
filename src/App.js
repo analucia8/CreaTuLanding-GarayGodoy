ebcc2e5cc6be48d13c95a696b5bbe00a8cfe6e1b
analucia8 <analuciagaray@gmail.com>
2025-07-28 02:01:34 -0300

    subida de archivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+// src/App.jsx
 import './App.css';
+import NavBar from './components/NavBar';
+import Contenedor from './components/Contenedor';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Contenedor saludo="¡Hola Ana! Este es nuestro catálogo" />
+    </>
   );
 }
 
 export default App;
+
